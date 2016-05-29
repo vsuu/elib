89a1bd28d9316062c0e06acad75732129c44e2e0
vsuu <rftdmail@gmail.com>
2016-05-29 11:20:08 +0800

    modify ERR_WHERE with XSTR

--- elib2014/libbase.h
+++ elib2014/libbase.h
@@ -5,15 +5,15 @@
 #define __LIB_NAME_SPACE_BEGIN__  namespace __LIB_NAME__ {
 #define __LIB_NAME_SPACE_END__    }
 
-#define ELIB_STRINGLINE(x) #x
-#define ELIB_LINETOSTRING(x) ELIB_STRINGLINE(x)
-#define ERR_WHERE  "error at " __FILE__  ", line " ELIB_LINETOSTRING(__LINE__) " : "
+#define STR(x) #x
+#define XSTR(x) STR(x)
+
+#define ERR_WHERE  "error at " __FILE__  ", line " XSTR(__LINE__) " : "
 
 #define elif else if
 
 #define RNG(x) std::begin(x),std::end(x)
 
-#define STR(x) #x
-#define XSTR(x) STR(x)
+
 
 #endif // _LIB_BASE_H_RFT
